cee23b73e7c4e35a13a647646580dc7d0726fc02
Juan Camilo P <juan.camilo93@outlook.com>
2020-07-13 20:09:03 -0500

    First Commit

---
+++ wsVehicles/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace wsVehicles.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ wsVehicles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wsVehicles.WebApiApplication" Language="C#" %>
